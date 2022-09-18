829017972c13b0321e9f777688ed3ab356875f74
Azat Ilyazov <ilyazovazat@gmail.com>
2022-09-18 20:48:27 +0300

    mongo in progress

--- app/api/routes/annotators.py
+++ app/api/routes/annotators.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
-from fastapi import status
-from app.models.annotators import AnnotatorList, AnnotatorListImport, AnnotatorImportConfig
+from fastapi import APIRouter, Depends
+from fastapi import status, HTTPException
+from fastapi.encoders import jsonable_encoder
+from app.models.annotators import AnnotatorList, AnnotatorListImport, AnnotatorImportConfig, AnnotatorItem
+from app.db import get_database
 
 router = APIRouter(prefix="/annotators")
 
@@ -13,6 +15,29 @@ async def get_annotators():
         AnnotatorList: list of the available for user annotator
     """
     return {"name": "foo"}
+
+
+@router.post("/")
+async def add_one_annotator(annotator: AnnotatorItem, db=Depends(get_database)):
+    last_id = await db.annotators.insert_one(jsonable_encoder(annotator))
+    return last_id
+
+
+@router.get("/{item_id}", status_code=200)
+async def get_annotators_by_id(item_id: str, db=Depends(get_database)):
+    annotator = await db.annotators.find_one({"id": item_id})
+    if annotator:
+        return annotator
+    else:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Annotator wasn't found"
+        )
+
+
+@router.get("/test")
+async def test(commons: int = Depends(get_database)):
+    return commons
 
 
 @router.post("/import", status_code=status.HTTP_201_CREATED)
--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 from app.api.api import router
-
+from app.db import db
 app = FastAPI()
+
+API_URL = "mongodb://localhost:27017/"
+@app.on_event("startup")
+async def startup():
+    await db.connect_to_database(path=API_URL)
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.close_database_connection()
+
 app.include_router(router)
