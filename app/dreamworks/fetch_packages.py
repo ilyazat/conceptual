2aa4d8648b63123a1450b29f8e35338177c2c804
Azat Ilyazov <ilyazovazat@gmail.com>
2022-10-03 16:32:23 +0600

    rename vars

--- app/dreamworks/fetch_packages.py
+++ app/dreamworks/fetch_packages.py
@@ -10,8 +10,8 @@ from app.dreamworks.const import (
 )
 
 
-class PackagesList:
-    packages = {
+class DreamComponent:
+    components = {
         "annotator": ANNOTATOR_DIR_NAME,
         "assistant": ASSISTANT_DISTS_DIR_NAME,
         "skills": SKILLS_DIR_NAME,
@@ -22,38 +22,37 @@ class PackagesList:
             dream_root if isinstance(dream_root, Path) else Path(dream_root)
         )
 
-    def get_list(self, package: str, to_json: bool = False) -> Optional[list, str]:
+    def get_list(self, component_name: str, to_json: bool = False) -> Optional[list, str]:
         """
-        Fetch list of all items in the package in Dream
+        Fetch list of all items in the package in Dream Component
 
         Args:
-            package: name of the package, i.e. 'annotator', 'assistant'
+            component_name: name of the component, i.e. 'annotator', 'assistant'
             to_json: if True method returns json string
 
         Returns:
-            list_of_packages: python list or json string of packages in Dream package
+            list_of_packages_in_component: list or json string of packages in Dream package
         """
 
-        package_path: Path = self.dream_root / self.__class__.packages[package]
-        list_of_package_names = []
+        component_path: Path = self.dream_root / self.__class__.components[component_name]
+        list_of_packages_in_component = []
 
-        for item in package_path.iterdir():
-            list_of_package_names.append(item.name)
+        for item in component_path.iterdir():
+            list_of_packages_in_component.append(item.name)
 
         if to_json:
-            list_of_package_names = json.dumps({package: list_of_package_names})
+            list_of_packages_in_component = json.dumps({component_name: list_of_packages_in_component})
 
-        return list_of_package_names
+        return list_of_packages_in_component
 
     def load_to_db(self) -> None:
         """
-        Loads packages to MongoDB
+        Loads packages of component to MongoDB
         """
         db = get_database()
 
-        for package in self.__class__.packages:
-            items = self.get_list(package)
+        for component in self.__class__.components:
+            items = self.get_list(component)
 
             for item in items:
-                await getattr(db, package).insert_one({"id": None, "name": item})
-
+                await getattr(db, component).insert_one({"id": None, "name": item})
